bf5d7f715289fe5ae41a49c1e924ec130998ae7f
MateuszStasak <mstasaq@gmail.com>
2023-02-16 19:26:40 +0100

    Added backend graphql, prisma types and users feature

---
+++ libs/api/feature-user/src/lib/user.resolver.ts
@@ -0,0 +1,34 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User, CreateOneUserArgs, FindUniqueUserArgs, UpdateOneUserArgs, DeleteOneUserArgs } from '@e-commerce-app/api/generated-db-types';
+
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => User)
+  createUser(@Args() createOneUserArgs: CreateOneUserArgs) {
+    return this.userService.create(createOneUserArgs);
+  }
+
+  @Query(() => [User])
+  users(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Query(() => User)
+  user(@Args() findUniqueUserArgs: FindUniqueUserArgs) {
+    return this.userService.findOne(findUniqueUserArgs);
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args() updateOneUserArgs: UpdateOneUserArgs) {
+    return this.userService.update(updateOneUserArgs);
+  }
+
+  @Mutation(() => User)
+  removeUser(@Args() deleteOneUserArgs: DeleteOneUserArgs) {
+    return this.userService.remove(deleteOneUserArgs);
+  }
+}
