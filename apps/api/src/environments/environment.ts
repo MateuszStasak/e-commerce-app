ef6dadaa680b2ac2fb89d880457d8e776a5734d5
MateuszStasak <mstasaq@gmail.com>
2023-04-25 20:18:47 +0200

    update env variables

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -3,10 +3,10 @@ import * as process from 'node:process'
 export const apiEnv: IApiEnv = {
   isProd: process.env.ECOM_ENV === 'production',
   api: {
-    port: +process.env.ECOM_API_PORT || 80
+    port: 333
   },
   db: {
-    url: process.env.ECOM_DB_URL
+    url: "postgresql://postgres:KiaraINala12@db.seigdfcnfbuhlupdgjsm.supabase.co:5432/postgres?schema=public"
   }
 }
 
